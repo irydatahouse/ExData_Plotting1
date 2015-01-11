b29c5ce77b0dcc9cf49fd31fc78fe09798d6f16a
Iryna <iryhouse@gmail.com>
2015-01-11 19:42:39 +0200

    project 1 files

---
+++ plot3.R
@@ -0,0 +1,23 @@
+
+power.data <- read.csv("household_power_consumption.txt",sep=";", header=TRUE, as.is=TRUE)
+
+power.data$Date <- as.Date(power.data$Date, format="%d/%m/%Y")
+
+power.feb <- subset(power.data, Date=="2007-02-01" | Date=="2007-02-02")
+power.feb$DateTime <- as.POSIXct(strptime(paste(as.character(power.feb$Date),as.character(power.feb$Time)), "%Y-%m-%d %H:%M:%S"))
+power.feb$Global_active_power <- as.numeric(power.feb$Global_active_power)
+power.feb$Sub_metering_1 <- as.numeric(power.feb$Sub_metering_1)
+power.feb$Sub_metering_2 <- as.numeric(power.feb$Sub_metering_2)
+power.feb$Sub_metering_3 <- as.numeric(power.feb$Sub_metering_3)
+power.feb$Voltage <- as.numeric(power.feb$Voltage)
+
+par(mfrow = c(1, 1))
+plot(power.feb$DateTime, power.feb$Sub_metering_1, type ='l', xlab="", ylab="Energy Sub metering")
+
+lines(power.feb$DateTime, power.feb$Sub_metering_2, col="red")
+lines(power.feb$DateTime, power.feb$Sub_metering_3, col="blue")
+
+legends.met=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+legend("topright", lty=1, lwd=1, col = c("black", "blue", "red"), legend = legends.met, cex=0.8)
+dev.copy(png, file = "plot3.png", width = 480, height = 480)
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,27 @@
+
+power.data <- read.csv("household_power_consumption.txt",sep=";", header=TRUE, as.is=TRUE)
+
+power.data$Date <- as.Date(power.data$Date, format="%d/%m/%Y")
+
+power.feb <- subset(power.data, Date=="2007-02-01" | Date=="2007-02-02")
+power.feb$DateTime <- as.POSIXct(strptime(paste(as.character(power.feb$Date),as.character(power.feb$Time)), "%Y-%m-%d %H:%M:%S"))
+power.feb$Global_active_power <- as.numeric(power.feb$Global_active_power)
+power.feb$Sub_metering_1 <- as.numeric(power.feb$Sub_metering_1)
+power.feb$Sub_metering_2 <- as.numeric(power.feb$Sub_metering_2)
+power.feb$Sub_metering_3 <- as.numeric(power.feb$Sub_metering_3)
+power.feb$Voltage <- as.numeric(power.feb$Voltage)
+
+par(mfrow = c(2, 2))
+plot(power.feb$DateTime, power.feb$Global_active_power, type ='l', xlab="", ylab="Global Active Power")
+plot(power.feb$DateTime, power.feb$Voltage, type ='l', xlab="datetime", ylab="Voltage")
+plot(power.feb$DateTime, power.feb$Sub_metering_1, type ='l', xlab="", ylab="Energy Sub metering")
+
+lines(power.feb$DateTime, power.feb$Sub_metering_2, col="red")
+lines(power.feb$DateTime, power.feb$Sub_metering_3, col="blue")
+
+legends.met=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+legend("topright", lty=1, lwd=1, col = c("black", "blue", "red"), legend = legends.met, cex=0.5, bty="n")
+
+plot(power.feb$DateTime, power.feb$Global_reactive_power, type ='l', xlab="datetime", ylab="Global_reactive_power")
+dev.copy(png, file = "plot4.png", width = 480, height = 480)
+dev.off()
